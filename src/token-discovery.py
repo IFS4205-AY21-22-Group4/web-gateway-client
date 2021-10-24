0828232a701a945b31241f22009524d785d95169
Emkay16 <mabelkohjx@gmail.com>
2021-10-24 15:59:10 +0800

    Modify token-discovery.py for MacOS devices
    * MacOS script addr provides UUID not MAC address
    * If OS is detected to be macOS, connect to token to listen to token
      message (MAC address)

--- src/token-discovery.py
+++ src/token-discovery.py
@@ -1,15 +1,30 @@
 from adafruit_ble import BLERadio
+from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
+from adafruit_ble.services.gmsservice import GMS
+import platform
 import json
 
+
+def connect_token(entry, radio):
+    if GMS in entry.services:
+        GMS_connection = radio.connect(entry)
+        if GMS_connection and GMS_connection.connected:
+            response = GMS_connection[GMS].readline().decode("utf-8")
+    GMS_connection.disconnect()
+    return response
 
 def discover_tokens():
     radio = BLERadio()
     tokens = set()
-    for entry in radio.start_scan(timeout=2, minimum_rssi=-80):
+    for entry in radio.start_scan(ProvideServicesAdvertisement, timeout=2, minimum_rssi=-80):
         addr = entry.address
         name = entry.complete_name
-        if addr not in tokens and name != None and "Thing" in name:
-            tokens.add(addr.string)
+        if addr not in tokens and name != None and "IFS4205" in name:
+            if platform.system() == 'Darwin':
+                addr = connect_token(entry, radio)
+                tokens.add(addr)
+            else:
+                tokens.add(addr.string)
 
     return tokens
 
